0b6e855a08ac07bef57a86f2909ffff65a151b3d
sreeram <vnj2101@columbia.edu>
2015-05-21 17:38:17 -0400

    Testing feed,vote,results methods

--- service/ThisOrThat/application.py
+++ service/ThisOrThat/application.py
@@ -1,14 +1,31 @@
+
+import datetime
+import json
+
 from flask import Flask
 from flask import request
+from boto.sqs.message import Message
+
+" importing modules of the current project "
+from results import send_my_results, send_others_results
+from vote import set_my_decision
+from getSQSConn import create_post_queue
+from db_table_key_names import Post
+from create_post_worker import create_post
 import store_data, feed, vote, friends
+from util import get_epoch_now
+
+
+
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
     
-@app.route('/user_info')
+@app.route('/user_info',methods=["POST"])
 def store_user_info():
     print "VALUE : ",request.form
     uid = request.form("uid")
@@ -23,7 +40,7 @@ def store_tags():
 def get_feed(user_id):
     return feed.send_feed(int(user_id))
 
-@app.route("/posts/vote/<user_id>/<post_id>/<option_id>", methods=["POST"])
+@app.route("/posts/vote/<user_id>/<post_id>/<option_id>", methods=["GET","POST"])
 def place_vote(user_id, post_id, option_id):
     return vote.set_vote(int(user_id), int(post_id), int(option_id))
 
@@ -41,13 +58,64 @@ def get_friends(user_id, tag_id):
 
 ####################################################
 
+@app.route("/posts/decision/<user_id>/<post_id>/<option_id>",methods=["GET","POST"])
+def place_my_decision(user_id,post_id,option_id):
+    set_my_decision(int(user_id),int(post_id),int(option_id))
+    return "success"
+
+@app.route("/results/myResults/<user_id>")
+def get_my_results(user_id):
+    return send_my_results(int(user_id))
+
+@app.route("/results/othersResults/<user_id>")
+def get_others_results(user_id):
+    return send_others_results(int(user_id))
+
+@app.route("/posts/<user_id>",methods=["POST"])
+def insert_post_to_queue(user_id):
+    " @todo - return a 200 success message ; add methods = ['POST']"
+    my_data = json.loads(request.data)
+    post_dict = {Post.get_creator_key():int(user_id)}
+    post_dict[Post.get_creation_time_key()] = get_epoch_now()
+    reqd_key_mappings = {
+                         Post.get_this_img_key():"this_img",
+                         Post.get_that_img_key():"that_img",
+                         Post.get_this_txt_key():"this_txt",
+                         Post.get_that_txt_key():"that_txt",
+                         Post.get_tags_key():"tags",
+                         Post.get_friends_key():"friends",
+                         Post.get_exp_time_key():"exp_time"
+                        }
+    for _key,mod_key in reqd_key_mappings.iteritems():
+        try:
+            post_dict[_key] = my_data[mod_key]
+        except KeyError,e:
+            print mod_key
+            print _key
+            print e
+            return "failure"
+    
+    #===========================================================================
+    # " code that puts a post_dict into a SQS queue"
+    # post_dict_message = Message()
+    # post_dict_message.set_body(json.dumps(post_dict))
+    # create_post_queue.write(post_dict_message)
+    #===========================================================================
+    create_post(post_dict)
+    return "success"
+    
+
+
 @app.errorhandler(500)
 def internal_error(error):
+    " @todo - log this error "
     return "500 error"
 
 @app.errorhandler(404)
 def not_found(error):
     return "404 error"
+
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0", port = 5000, debug=True)
---
+++ service/ThisOrThat/create_post_worker.py
@@ -0,0 +1,90 @@
+import json
+import threading
+import traceback
+import time
+
+import requests
+
+from getSQSConn import create_post_queue, stateful_URN, stateful_token, stateful_counter_name
+from db_schema import post as post_db_schema
+from db_table_key_names import Post, Users
+from getDynamoDB import user_db, post_db, get_user_item
+
+pool_size = 10
+delay = 1
+
+def thread_job(delay,thread_name):
+    global create_post_queue
+    while(True):        
+        rs=create_post_queue.get_messages(1)
+        if (len(rs) > 0):
+            if create_post_queue.delete_message(rs[0]):
+                _message = rs[0].get_body()
+                post_query = json.loads(_message)
+                create_post(post_query)
+        else:
+            print "thread:%s ; no messages in queue ; sleeping for %d secs"%(thread_name,delay)
+        time.sleep(delay)
+
+def get_latest_post_id():
+    payload = {'value':1}
+    headers = {'Host': 'www.stateful.co', 'X-Sttc-URN': stateful_URN, 
+               'X-Sttc-Token': stateful_token, 'Accept': 'text/plain'}
+    increment_url = "http://www.stateful.co/c/%s/inc"%(stateful_counter_name)
+    resp = requests.get(increment_url,params=payload,headers=headers)
+    if resp.status_code == 200:
+        latest_id = int(resp.text)
+        return latest_id
+    else:
+        "todo - handle this else block ; raise an exception ;or log this error"
+        pass
+
+def create_post(post_query):
+    latest_post_id = get_latest_post_id()
+    new_post = dict(post_query)
+    user_id = new_post[Post.get_creator_key()]
+    
+    " initializing the POST table keys whose data is not received from front-end"
+    new_post[Post.get_this_count_key()] = 0
+    new_post[Post.get_that_count_key()] = 0
+    new_post[Post.get_complete_key()] = False
+    new_post[Post.get_decision_key()] = None
+    new_post[Post.get_id_key()] = latest_post_id
+    friends_id_li = new_post[Post.get_friends_key()]
+    new_post[Post.get_friends_key()] = []
+    for index,friend_id in enumerate(friends_id_li):
+        li = {}
+        li[Post.get_friends_frd_id_key()] = friend_id
+        li[Post.get_friends_vote_key()] = None
+        li[Post.get_friends_vote_time_key()] = None
+        li[Post.get_friends_resp_time_key()] = None
+        new_post[Post.get_friends_key()].append(li)
+
+        "adding post-id to unvoted-posts field of users table"
+        friend_user_item = get_user_item(friend_id)
+        friend_user_item[Users.get_unvoted_posts_key()].append(latest_post_id)
+        friend_user_item.partial_save()
+
+    
+    " persisting the post-item into post table"
+    post_db.put_item(data=new_post)
+    
+    " adding post-id into created_posts field of users table"
+    user_item = get_user_item(user_id)
+    user_item[Users.get_created_posts_key()].append(latest_post_id)
+    user_item.partial_save()
+    
+def main():
+    try:
+        threads = []
+        for i in range(1, pool_size+1):
+            thread_name = "Thread %d"%(i,)
+            t = threading.Thread(target=thread_job,name=thread_name,args=(delay,thread_name))
+            threads.append(t)
+            t.start()
+    except:
+        print traceback.format_exc()
+        print "Unable to start threads"
+
+if __name__ == '__main__':
+    main()
